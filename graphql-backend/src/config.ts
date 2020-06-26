9c9f9e8866afda9546107e263d7f67dfa0e5bc17
horm <horm@horm.codes>
2020-06-26 15:04:54 +0200

    Extract backend database names to env vars

--- graphql-backend/src/config.ts
+++ graphql-backend/src/config.ts
@@ -1,12 +1,15 @@
 import { config } from 'dotenv';
+import { Config } from './types';
 
 config();
 
-const CONFIG = {
+const CONFIG: Config = {
   PORT: process.env.PORT || '5000',
   MY_OKAY_CASH_WS_API_URL: process.env.MY_OKAY_CASH_WS_API_URL || 'wss://myokaycash-backend.herokuapp.com/graphql',
   MY_OKAY_CASH_HTTPS_API_URL:
     process.env.MY_OKAY_CASH_HTTPS_API_URL || 'https://myokaycash-backend.herokuapp.com/graphql',
+  DATABASE_NAME: 'pretty_things.db',
+  TEST_DATABASE_NAME: 'pretty_things_test.db',
 };
 
 export default CONFIG;
--- graphql-backend/src/database/index.ts
+++ graphql-backend/src/database/index.ts
@@ -1,7 +1,8 @@
 import Loki from 'lokijs';
 import COLLECTIONS from './collections';
+import CONFIG from '../config';
 
-export function getDatabase(databaseName: string = 'pretty_things.db'): LokiConstructor {
+export function getDatabase(databaseName: string = CONFIG.DATABASE_NAME): LokiConstructor {
   const database = new Loki(databaseName);
 
   COLLECTIONS.forEach((collection) => {
