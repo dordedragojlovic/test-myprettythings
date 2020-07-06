03e054e73033c63d309ea163a8ea9728c0c94ebd
djordje <djordje@snowball.digital>
2020-07-06 16:09:18 +0200

    added 'Items in cart' section to purchase-validation page

--- web/page-components/purchase-validation/view/index.tsx
+++ web/page-components/purchase-validation/view/index.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { FormStateHandler } from '../types';
-import { PageContainer } from './styles';
+import { PageContainer, OrderWrapper, OrderList, Item, CardContainer } from './styles';
 import CreditCard from './credit-card';
+import Card from 'components/card';
 import Popup from './popup';
 
 function PurchaseValidationView({ form, waitingForPayment }: { form: FormStateHandler; waitingForPayment: boolean }) {
   return (
     <>
       {waitingForPayment && <Popup />}
+      <OrderWrapper>
+        <h3>Items in cart:</h3>
+        <OrderList>
+          <Item>
+            <CardContainer>
+              <Card heigh={40} width={35} showText={false}/>
+            </CardContainer>
+            <p>Pretty chair</p>
+          </Item>
+          <p>123 $</p>
+        </OrderList>
+      </OrderWrapper>
       <form onSubmit={form.handleSubmit}>
         <PageContainer>
           <CreditCard form={form} />
