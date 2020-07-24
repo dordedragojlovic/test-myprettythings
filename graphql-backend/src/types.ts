bd46ca0ead236588883eda4afd2b522ec7dce12c
Horm <matej@okaythis.com>
2020-07-24 16:47:11 +0200

    BE: Convert type definitions to interfaces

--- graphql-backend/src/types.ts
+++ graphql-backend/src/types.ts
@@ -2,43 +2,43 @@ import { PaymentInfo, PurchaseInput, PurchaseStatus } from './generated/graphql-
 import { Observable } from 'zen-observable-ts';
 import { ResolverFn } from 'graphql-subscriptions';
 
-export type Config = {
+export interface Config {
   PORT: string;
   MY_OKAY_CASH_WS_API_URL: string;
   MY_OKAY_CASH_HTTPS_API_URL: string;
   DATABASE_NAME: string;
   TEST_DATABASE_NAME: string;
-};
+}
 
-export type Repositories = {
+export interface Repositories {
   purchase: PurchaseRepository;
-};
+}
 
-export type PurchaseRepository = {
+export interface PurchaseRepository {
   get: (id: string) => PurchaseStatus;
   createPurchase: (purchase: PurchaseInput, paymentId: string, paid: boolean) => PurchaseStatus;
   updatePurchase: (id: string, fieldsToUpdate: { paid: boolean; confirmed: boolean }) => PurchaseStatus;
   reset: () => void;
-};
+}
 
-export type BankService = {
+export interface BankService {
   postPayment: (
     paymentInfo: PaymentInfo,
     amount: number,
     currency: string,
   ) => Promise<{ id: string; confirmed: boolean }>;
   getPaymentObservable: (paymentId: string, paymentInfo: PaymentInfo) => Observable<boolean>;
-};
+}
 
-export type PurchasePaidStream = {
+export interface PurchasePaidStream {
   name: string;
   publish: (input: { paid: boolean; id: string }) => void;
   resolver: {
     resolve: (payload: { purchasePaid: { id: string; paid: boolean } }) => boolean;
     subscribe: ResolverFn;
   };
-};
+}
 
-export type Streams = {
+export interface Streams {
   purchasePaid: PurchasePaidStream;
-};
+}
